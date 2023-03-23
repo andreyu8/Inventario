bdb430293b2c7dfd89ad9b0ace8cbe07c9e5e7bd
Andrei <41924851+andreyu8@users.noreply.github.com>
2023-03-22 22:00:40 -0600

    Cambios campos y tablas
    Cambios campos y tablas

---
+++ src/com/seidor/inventario/adapter/ClientAdapter.java
@@ -0,0 +1,26 @@
+package com.seidor.inventario.adapter;
+
+import com.seidor.inventario.model.Cliente;
+
+public class ClientAdapter {
+	
+	private Cliente cliente;
+	private Boolean falgActive;
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Boolean getFalgActive() {
+		return falgActive;
+	}
+
+	public void setFalgActive(Boolean falgActive) {
+		this.falgActive = falgActive;
+	}
+
+}
