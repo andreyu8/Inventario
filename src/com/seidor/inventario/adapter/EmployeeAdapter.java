977b59e4054d2917ebd9cb6bc83399170cb55a8f
Cristian Sanchez Flores <andygign40@gmail.com>
2023-04-21 11:19:28 -0600

    crud empleados

--- src/com/seidor/inventario/adapter/EmployeeAdapter.java
+++ src/com/seidor/inventario/adapter/EmployeeAdapter.java
@@ -5,6 +5,7 @@ import com.seidor.inventario.model.Empleado;
 public class EmployeeAdapter {
 	
 	private Empleado empleado;
+	private Boolean falgActive;
 
 	public Empleado getEmpleado() {
 		return empleado;
@@ -12,5 +13,13 @@ public class EmployeeAdapter {
 
 	public void setEmpleado(Empleado employee) {
 		this.empleado = employee;
+	}
+
+	public Boolean getFalgActive() {
+		return falgActive;
+	}
+
+	public void setFalgActive(Boolean falgActive) {
+		this.falgActive = falgActive;
 	}
 }
--- src/com/seidor/inventario/controller/EmployeeController.java
+++ src/com/seidor/inventario/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.seidor.inventario.controller;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.zkoss.spring.SpringUtil;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.ListModelList;
@@ -15,6 +16,7 @@ import com.seidor.inventario.manager.EmployeeManager;
 import com.seidor.inventario.model.Empleado;
 import com.seidor.inventario.navigation.NavigationControl;
 import com.seidor.inventario.navigation.NavigationState;
+import com.seidor.inventario.navigation.NavigationStates;
 
 public class EmployeeController {
 
@@ -40,7 +42,6 @@ public class EmployeeController {
 		this.navigationControl = navigationControl;
 	}
 
-	
 	public void loadEmployee(Combobox combo) {
 		ArrayList<Empleado> empleados = this.employeeManager.getAll();
 		if (empleados != null) {
@@ -50,7 +51,6 @@ public class EmployeeController {
 		}
 	}
 	
-	//buscar
 	public void search(Listbox lb, EmployeeSearchAdapter esa, NavigationState state){
 		ArrayList<Empleado> employee = this.employeeManager.search(esa);
 		
@@ -58,7 +58,6 @@ public class EmployeeController {
 		lb.setModel(model);
 	}
 	
-	//mostrar
 	public void show(Listbox lb, NavigationState state, Component win){
 		if (lb.getSelectedIndex() >= 0) {
 			Empleado employee = (Empleado)lb.getModel().getElementAt(lb.getSelectedIndex());
@@ -79,7 +78,6 @@ public class EmployeeController {
 		}
 	}
 	
-	//leer
 	public EmployeeAdapter read(Integer employeeId){
 		EmployeeAdapter u = new EmployeeAdapter();
 		
@@ -90,7 +88,6 @@ public class EmployeeController {
 		return u;
 	}
 	
-	//leer para editar
 	public EmployeeAdapter readForEdit(Integer employeeId){
 		EmployeeAdapter u = new EmployeeAdapter();
 		
@@ -111,7 +108,6 @@ public class EmployeeController {
 		return u;
 	}
 	
-	//eliminar
 	public void delete(EmployeeAdapter u, NavigationState state, Component win){		
 		this.employeeManager.delete(u.getEmpleado());
 		
@@ -120,6 +116,40 @@ public class EmployeeController {
 		state.removeLastBreadCrumbs();
 		state.removeLastBreadCrumbs();
 		this.navigationControl.changeView(win, state);
+	}
+
+	public void save(EmployeeAdapter pa, NavigationState state, Component win){
+		
+		pa.getEmpleado().setActivo(1);
+		
+		this.employeeManager.save(pa.getEmpleado());
+		
+		state.setDetailIdentifier(pa.getEmpleado().getNombre());
+		state.setUri("/WEB-INF/zul/employee/main.zul");
+		state.removeLastBreadCrumbs();
+		state.appendBreadCrumbsPath(pa.getEmpleado().getNombre());
+		this.navigationControl.changeView(win, state);
+	}
+	
+	public void update(EmployeeAdapter pa, NavigationState state, Component win){
+		
+		
+		if (pa.getFalgActive())
+			pa.getEmpleado().setActivo(1);
+		else
+			pa.getEmpleado().setActivo(0);
+		
+		this.employeeManager.update(pa.getEmpleado());
+			
+		NavigationStates navStates = (NavigationStates)SpringUtil.getBean("navigationStates");
+		NavigationState prev = navStates.getPreviousOriginal();
+		prev.removeLastBreadCrumbs();
+		prev.appendBreadCrumbsPath(pa.getEmpleado().getNombre());
+		if (prev.getDetailLabels() != null) {
+			prev.getDetailLabels().set(prev.getDetailIndex(), pa.getEmpleado().getNombre());
+		}
+		
+		this.navigationControl.changeToPrevious(win);
 	}
 	
 }
